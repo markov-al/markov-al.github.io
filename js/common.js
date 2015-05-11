e5cf41f212bb4f3945fbcfaf4af7fa87e347c980
markov-al <alexandr@Alexandrs-Air.Dlink>
2015-05-11 17:01:49 +0300

    yo
    yo

--- js/common.js
+++ js/common.js
@@ -12,6 +12,24 @@ $(document).ready(function() {
 		$(".offers-cta .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
+	$(function(){
+		$(".second form .input-group input").focusout(function(){
+			var text_val = $(this).val();
+			if(text_val === "") {
+				$(this).removeClass("has-value is-active");
+			} else {
+				$(this).addClass("has-value is-active");
+			}
+		});
+
+		$(".second form .input-group input").focus(function(){
+			$(this).addClass("is-active");
+		});
+		$(".second form .input-group input").focusout(function(){
+			$(this).removeClass("is-active");
+			});
+	});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
