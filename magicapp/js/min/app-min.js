470fb8bfede1a45fce736284e9cbb0aa3e3924c9
markov-al <alexandr@Alexandrs-Air.connectify>
2015-05-16 18:27:48 +0300

    v
    v

---
+++ magicapp/js/min/app-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".icon-menu").click(function(){$(".nav").animate({right:"0px"},200),$("body").animate({right:"200px"},200).addClass("overflow")}),$("..icon-cancel").click(function(){$(".nav").animate({right:"-200px"},200),$("body").animate({right:"0px"},200).removeClass("overflow")})});
